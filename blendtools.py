07b0714aa0ced3a75efc4d05c2ee5c3f12dbb371
M4X4 <63986908+M4X40@users.noreply.github.com>
2023-07-13 22:19:16 -0500

    Update blendtools.py

--- blendtools.py
+++ blendtools.py
@@ -24,7 +24,7 @@ def udump_importer():
     
     def complete_popup():
         popup = tk.Toplevel(importer_window)
-        popup.wm_title("Done!")
+        popup.wm_title("UDump Importer")
         popup.geometry("150x75")
         popup.config(bg="#0d1117")
         msg = tk.Label(popup, text="Copied to Clipboard!", bg="#0d1117", fg="#c9d1d9")
@@ -49,17 +49,37 @@ def udump_importer():
     no_button.pack(side="right", expand=True)
 
 def find_replace_materials():
-    print("Find/Replace Materials button clicked")
+    script = str(urlopen('https://raw.githubusercontent.com/M4X40/BlenderTools/scripts/findreplacematerial.py').read().decode('utf8')) 
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardText(script, 13)
+    win32clipboard.CloseClipboard()
+    frm_window = tk.Toplevel(root)
+    frm_window.title("Find Replace Material")
+    frm_window.geometry("150x75")
+    frm_window.config(bg="#0d1117")
+    msg = tk.Label(frm_window, text="Copied to Clipboard!", bg="#0d1117", fg="#c9d1d9")
+    msg.pack()
+    def okay():
+        frm_window.destroy()
+    ok_button = tk.Button(frm_window, text="Okay", command=okay, bg="#c9d1d9")
+    ok_button.pack(expand=True)
+
 def mat_uv_duplicator():
     print("Mat/UV Duplicator button clicked")
+
 def find_node_type():
     print("Find Node Type button clicked")
+
 def group_to_pbsdf():
     print("Group to PBSDF button clicked")
+
 def split_orm():
     print("Split ORM button clicked")
+
 def remove_mat_clones():
     print("Remove Mat Clones button clicked")
+
 
 root = tk.Tk()
 root.title("M4X4's Blender Tools")
@@ -69,22 +89,22 @@ root.config(bg="#0d1117")
 udump_importer_button = tk.Button(root, text="UDump Importer", command=udump_importer, bg="#c9d1d9")
 udump_importer_button.pack()
 
-find_replace_materials_button = tk.Button(root, text="Find/Replace Materials", command=find_replace_materials, bg="#ea99a2")
+find_replace_materials_button = tk.Button(root, text="Find/Replace Materials", command=find_replace_materials, bg="#c9d1d9")
 find_replace_materials_button.pack()
 
-mat_uv_duplicator_button = tk.Button(root, text="Mat-UV Duplicator", command=mat_uv_duplicator, bg="#ea99a2")
-mat_uv_duplicator_button.pack()
+# mat_uv_duplicator_button = tk.Button(root, text="Mat-UV Duplicator", command=mat_uv_duplicator, bg="#ea99a2")
+# mat_uv_duplicator_button.pack()
 
-find_node_type_button = tk.Button(root, text="Find Node Type", command=find_node_type, bg="#ea99a2")
-find_node_type_button.pack()
+# find_node_type_button = tk.Button(root, text="Find Node Type", command=find_node_type, bg="#ea99a2")
+# find_node_type_button.pack()
 
-group_to_pbsdf_button = tk.Button(root, text="Group to PBSDF", command=group_to_pbsdf, bg="#ea99a2")
-group_to_pbsdf_button.pack()
+# group_to_pbsdf_button = tk.Button(root, text="Group to PBSDF", command=group_to_pbsdf, bg="#ea99a2")
+# group_to_pbsdf_button.pack()
 
-split_orm_button = tk.Button(root, text="Split ORM", command=split_orm, bg="#ea99a2")
-split_orm_button.pack()
+# split_orm_button = tk.Button(root, text="Split ORM", command=split_orm, bg="#ea99a2")
+# split_orm_button.pack()
 
-remove_mat_clones_button = tk.Button(root, text="Remove Mat Clones", command=remove_mat_clones, bg="#ea99a2")
-remove_mat_clones_button.pack()
+# remove_mat_clones_button = tk.Button(root, text="Remove Mat Clones", command=remove_mat_clones, bg="#ea99a2")
+# remove_mat_clones_button.pack()
 
 root.mainloop()
