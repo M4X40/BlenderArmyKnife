8d1205c9b8a0377b9ca0186201869694e56a2021
M4X4 <63986908+M4X40@users.noreply.github.com>
2023-07-14 19:11:25 -0500

    Add files via upload

---
+++ BlenderArmyKnife/BlenderArmyKnife.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace BlenderArmyKnife
+{
+    partial class BlenderArmyKnife
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UDumpImporter = new System.Windows.Forms.Button();
+            this.FindReplaceMat = new System.Windows.Forms.Button();
+            this.MatUVDupe = new System.Windows.Forms.Button();
+            this.FindNodeType = new System.Windows.Forms.Button();
+            this.GroupToPBSDF = new System.Windows.Forms.Button();
+            this.RemoveMatClones = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UDumpImporter
+            // 
+            this.UDumpImporter.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UDumpImporter.AutoSize = true;
+            this.UDumpImporter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(209)))), ((int)(((byte)(217)))));
+            this.UDumpImporter.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.UDumpImporter.Location = new System.Drawing.Point(88, 0);
+            this.UDumpImporter.Margin = new System.Windows.Forms.Padding(0);
+            this.UDumpImporter.Name = "UDumpImporter";
+            this.UDumpImporter.Size = new System.Drawing.Size(115, 28);
+            this.UDumpImporter.TabIndex = 1;
+            this.UDumpImporter.Text = "UDump Importer";
+            this.UDumpImporter.UseVisualStyleBackColor = false;
+            this.UDumpImporter.Click += new System.EventHandler(this.UDumpImporter_Click);
+            // 
+            // FindReplaceMat
+            // 
+            this.FindReplaceMat.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FindReplaceMat.AutoSize = true;
+            this.FindReplaceMat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(209)))), ((int)(((byte)(217)))));
+            this.FindReplaceMat.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FindReplaceMat.Location = new System.Drawing.Point(60, 28);
+            this.FindReplaceMat.Margin = new System.Windows.Forms.Padding(0);
+            this.FindReplaceMat.Name = "FindReplaceMat";
+            this.FindReplaceMat.Size = new System.Drawing.Size(171, 28);
+            this.FindReplaceMat.TabIndex = 2;
+            this.FindReplaceMat.Text = "Find/Replace Materials";
+            this.FindReplaceMat.UseVisualStyleBackColor = false;
+            this.FindReplaceMat.Click += new System.EventHandler(this.FindReplaceMat_Click);
+            // 
+            // MatUVDupe
+            // 
+            this.MatUVDupe.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MatUVDupe.AutoSize = true;
+            this.MatUVDupe.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(209)))), ((int)(((byte)(217)))));
+            this.MatUVDupe.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.MatUVDupe.Location = new System.Drawing.Point(77, 56);
+            this.MatUVDupe.Margin = new System.Windows.Forms.Padding(0);
+            this.MatUVDupe.Name = "MatUVDupe";
+            this.MatUVDupe.Size = new System.Drawing.Size(136, 28);
+            this.MatUVDupe.TabIndex = 3;
+            this.MatUVDupe.Text = "Mat-UV Duplicator";
+            this.MatUVDupe.UseVisualStyleBackColor = false;
+            this.MatUVDupe.Click += new System.EventHandler(this.MatUVDupe_Click);
+            // 
+            // FindNodeType
+            // 
+            this.FindNodeType.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FindNodeType.AutoSize = true;
+            this.FindNodeType.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(209)))), ((int)(((byte)(217)))));
+            this.FindNodeType.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FindNodeType.Location = new System.Drawing.Point(88, 84);
+            this.FindNodeType.Margin = new System.Windows.Forms.Padding(0);
+            this.FindNodeType.Name = "FindNodeType";
+            this.FindNodeType.Size = new System.Drawing.Size(115, 28);
+            this.FindNodeType.TabIndex = 4;
+            this.FindNodeType.Text = "Find Node Type";
+            this.FindNodeType.UseVisualStyleBackColor = false;
+            this.FindNodeType.Click += new System.EventHandler(this.FindNodeType_Click);
+            // 
+            // GroupToPBSDF
+            // 
+            this.GroupToPBSDF.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GroupToPBSDF.AutoSize = true;
+            this.GroupToPBSDF.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(209)))), ((int)(((byte)(217)))));
+            this.GroupToPBSDF.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.GroupToPBSDF.Location = new System.Drawing.Point(53, 112);
+            this.GroupToPBSDF.Margin = new System.Windows.Forms.Padding(0);
+            this.GroupToPBSDF.Name = "GroupToPBSDF";
+            this.GroupToPBSDF.Size = new System.Drawing.Size(185, 28);
+            this.GroupToPBSDF.TabIndex = 5;
+            this.GroupToPBSDF.Text = "Group to Principled BSDF";
+            this.GroupToPBSDF.UseVisualStyleBackColor = false;
+            this.GroupToPBSDF.Click += new System.EventHandler(this.GroupToPBSDF_Click);
+            // 
+            // RemoveMatClones
+            // 
+            this.RemoveMatClones.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RemoveMatClones.AutoSize = true;
+            this.RemoveMatClones.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(209)))), ((int)(((byte)(217)))));
+            this.RemoveMatClones.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.RemoveMatClones.Location = new System.Drawing.Point(60, 140);
+            this.RemoveMatClones.Margin = new System.Windows.Forms.Padding(0);
+            this.RemoveMatClones.Name = "RemoveMatClones";
+            this.RemoveMatClones.Size = new System.Drawing.Size(171, 28);
+            this.RemoveMatClones.TabIndex = 6;
+            this.RemoveMatClones.Text = "Remove Material Clones";
+            this.RemoveMatClones.UseVisualStyleBackColor = false;
+            this.RemoveMatClones.Click += new System.EventHandler(this.RemoveMatClones_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(17)))), ((int)(((byte)(23)))));
+            this.ClientSize = new System.Drawing.Size(290, 168);
+            this.Controls.Add(this.RemoveMatClones);
+            this.Controls.Add(this.GroupToPBSDF);
+            this.Controls.Add(this.FindNodeType);
+            this.Controls.Add(this.MatUVDupe);
+            this.Controls.Add(this.FindReplaceMat);
+            this.Controls.Add(this.UDumpImporter);
+            this.Font = new System.Drawing.Font("JetBrainsMono Nerd Font Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Blender Army Knife";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button UDumpImporter;
+        private System.Windows.Forms.Button FindReplaceMat;
+        private System.Windows.Forms.Button MatUVDupe;
+        private System.Windows.Forms.Button FindNodeType;
+        private System.Windows.Forms.Button GroupToPBSDF;
+        private System.Windows.Forms.Button RemoveMatClones;
+    }
+}
+
---
+++ BlenderArmyKnife/BlenderArmyKnife.cs
@@ -0,0 +1,69 @@
+﻿using BlenderArmyKnife.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlenderArmyKnife
+{
+    public partial class BlenderArmyKnife : Form
+    {
+        public BlenderArmyKnife()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UDumpImporter_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(Resources.importer);
+            var popupWindow = new Popup();
+            popupWindow.Show();
+        }
+
+        private void FindReplaceMat_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(Resources.findreplacematerial);
+            var popupWindow = new Popup();
+            popupWindow.Show();
+        }
+
+        private void MatUVDupe_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(Resources.Mat_UV_Looping);
+            var popupWindow = new Popup();
+            popupWindow.Show();
+        }
+
+        private void FindNodeType_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(Resources.FindNodeType);
+            var popupWindow = new Popup();
+            popupWindow.Show();
+        }
+
+        private void GroupToPBSDF_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(Resources.GroupToPBSDF);
+            var popupWindow = new Popup();
+            popupWindow.Show();
+
+        }
+
+        private void RemoveMatClones_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(Resources.RemoveMaterialClones);
+            var popupWindow = new Popup();
+            popupWindow.Show();
+        }
+    }
+}
